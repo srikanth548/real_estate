937db5f97d7ece6543be55d29e963f9fd8f29d13
ManoharNuka <55279249+ManoharNuka@users.noreply.github.com>
2022-12-01 21:07:13 +0530

    initial commit
    1) Added terms and condition checkbox in Verify OTP screen
    2 Added validation like submitting OTP only if user checked Terms and conditions checkbox

--- RealEstate/app/src/main/java/com/gs/realestate/ui/login/ConfirmOtpFragment.kt
+++ RealEstate/app/src/main/java/com/gs/realestate/ui/login/ConfirmOtpFragment.kt
@@ -1,11 +1,13 @@
 package com.gs.realestate.ui.login
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.gs.realestate.R
@@ -19,11 +21,17 @@ import com.gs.realestate.util.SnackBarToast
 
 class ConfirmOtpFragment : BaseFragment() {
 
+    private lateinit var mContext: Context
     private var _binding: FragmentConfirmotpBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.mContext = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,11 +45,43 @@ class ConfirmOtpFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btConfirm.setOnClickListener {
-            otpVerification(it)
+            if(binding.cbTerms.isChecked){
+                otpVerification(it)
+            }else{
+                SnackBarToast.showErrorSnackBar(it, getString(R.string.str_please_check_terms_to_continue))
+            }
             //SnackBarToast.showErrorSnackBar(it, getString(R.string.invalidOtp))
             // findNavController().navigate(R.id.action_SignUpFragment_to_LoginFragment)
         }
 
+
+        binding.cbTerms.setOnClickListener {
+            binding.cbTerms.isChecked = false
+            showTermsAndConditionsDialog()
+        }
+    }
+
+    /*
+    * Method to show terms and conditions to user
+    * */
+    private fun showTermsAndConditionsDialog() {
+        val terms = this.resources.getStringArray(com.gs.realestate.R.array.terms)
+        val adapter = TermsAdapter(mContext, terms)
+        val builderSingle: AlertDialog.Builder = AlertDialog.Builder(mContext)
+        builderSingle.setTitle(getString(R.string.str_terms_conditions))
+
+        builderSingle.setAdapter(adapter) { _, _ ->
+
+        }
+        builderSingle.setPositiveButton("Accept") { dialog, _ ->
+            binding.cbTerms.isChecked = true
+            dialog.dismiss()
+        }
+        builderSingle.setNegativeButton("cancel") { dialog, _ ->
+            binding.cbTerms.isChecked = false
+            dialog.dismiss()
+        }
+        builderSingle.show()
     }
 
 
